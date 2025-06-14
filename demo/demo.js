a7c7fe8d1f3720b8135bc990c9a7de87a8d1a75c
Muhammad Azim <azeemamjad412@gmail.com>
2025-06-14 15:51:48 +0500

    add form

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,19 @@
 const inputInteger = require('..')
 
-const x = inputInteger()
+const input1 = inputInteger()
 
-document.body.append(x)
+title = 'demo form'
+subTitle = 'Please fill out the details:'
+
+const page = document.createElement('div')
+
+page.innerHTML = `
+    <h1> ${title} </h1>
+    <h2> ${subTitle} </h2>
+    <h3> Enter your age </h3>
+    <x></x>
+`
+
+page.querySelector('x').replaceWith(input1)
+
+document.body.append(page)
